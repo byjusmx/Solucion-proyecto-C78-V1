7dbe463e85ae5fae56c3348365206880bee2ca7d
byjusmx <87218747+byjusmx@users.noreply.github.com>
2021-11-30 12:47:10 -0600

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -21,28 +21,28 @@ export default class HomeScreen extends Component {
                 <ImageBackground source={require('../assets/stars.gif')} style={styles.backgroundImage}>
                     <View style={styles.titleBar}>
                         <Image source={require("../assets/main-icon.png")} style={{ width: 150, height: 150 }}></Image>
-                        <Text style={styles.titleText}>Stellar</Text>
                         <Text style={styles.titleText}>App</Text>
+                        <Text style={styles.titleText}>Estelar</Text>
                     </View>
 
                     <TouchableOpacity style={styles.routeCard} onPress={() =>
                         this.props.navigation.navigate("SpaceCraft")
                     }>
-                        <Text style={styles.routeText}>Spacecrafts</Text>
+                        <Text style={styles.routeText}>Naves espaciales</Text>
                         <Image source={require("../assets/space_crafts.png")} style={styles.routeImage}></Image>
                     </TouchableOpacity>
 
                     <TouchableOpacity style={styles.routeCard} onPress={() =>
                         this.props.navigation.navigate("StarMap")
                     }>
-                        <Text style={styles.routeText}>Star Map</Text>
+                        <Text style={styles.routeText}>Mapa estelar</Text>
                         <Image source={require("../assets/star_map.png")} style={styles.routeImage}></Image>
                     </TouchableOpacity>
 
                     <TouchableOpacity style={styles.routeCard} onPress={() =>
                         this.props.navigation.navigate("DailyPic")
                     }>
-                        <Text style={styles.routeText}>Daily Pictures</Text>
+                        <Text style={styles.routeText}>Imágenes diarias</Text>
                         <Image source={require("../assets/daily_pictures.png")} style={styles.routeImage}></Image>
                     </TouchableOpacity>
                 </ImageBackground>
@@ -83,7 +83,7 @@ const styles = StyleSheet.create({
         color: "white"
     },
     routeText: {
-        fontSize: 25,
+        fontSize: 20,
         fontWeight: "bold",
         color: '#D11583',
         justifyContent: "center",
